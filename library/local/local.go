80f44f88937eb2e845689c5581cdb0c7d2a710f4
Robert Carlsen <rwcarlsen@gmail.com>
2012-11-20 16:12:08 -0600

    added local hd implementation of a library backend

---
+++ library/local/local.go
@@ -0,0 +1,48 @@
+
+package local
+
+import (
+	"os"
+	"errors"
+	"io/ioutil"
+	"path/filepath"
+)
+
+type LocalBack struct {
+	Root string
+}
+
+func (lb *LocalBack) Put(path, name string, data []byte) error {
+	fullPath := filepath.Join(lb.Root, path, name)
+
+	f, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+
+	n, err := f.Write(data)
+	if n < len(data) {
+		return errors.New("local: failed to write entire data stream")
+	} else if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (lb *LocalBack) Exists(path, name string) bool {
+	fullPath := filepath.Join(lb.Root, path, name)
+	_, err := os.Stat(fullPath)
+	return err == nil
+}
+
+func (lb *LocalBack) Get(path, name string) ([]byte, error) {
+	fullPath := filepath.Join(lb.Root, path, name)
+
+	data, err := ioutil.ReadFile(fullPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
