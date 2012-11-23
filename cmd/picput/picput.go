dff8c8634f0343c2db7684a2c1549ed9cddd55e8
Robert Carlsen <rwcarlsen@gmail.com>
2012-11-22 23:19:16 -0600

    starting a command to easily put photos straight to s3

---
+++ cmd/picput/picput.go
@@ -0,0 +1,37 @@
+
+package main
+
+import (
+	"flag"
+
+	"github.com/rwcarlsen/gallery/piclib"
+	"github.com/rwcarlsen/gallery/piclib/amz"
+	"launchpad.net/goamz/aws"
+)
+
+func main() {
+	flag.Parse()
+
+	flag.Args()
+
+	// setup storage and piclib
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := amz.New(auth, aws.USEast)
+	lib := piclib.New(libName, db)
+
+	data, err := ioutil.ReadFile(sample)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// load and dump photo, thumbs, etc.
+	p, err := lib.AddPhoto(sample, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(p)
+}
