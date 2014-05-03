84eb86e2e855ddc86488a0473b75757532444fd8
Robert Carlsen <rwcarlsen@gmail.com>
2014-05-03 13:05:17 -0500

    created pic supercommand

--- cmd/pic/pic.go
+++ cmd/pic/pic.go
@@ -39,8 +39,8 @@ var cmds = map[string]CmdFunc{
 	"put": put,
 }
 
-func newFlagSet(cmd, args, desc string) {
-	fs := flag.NewFlagSet("put", flag.ContinueOnError)
+func newFlagSet(cmd, args, desc string) *flag.FlagSet {
+	fs := flag.NewFlagSet("put", flag.ExitOnError)
 	fs.Usage = func() {
 		log.Printf("Usage: pic %s [OPTION] %s\n%s\n", cmd, args, desc)
 		fs.PrintDefaults()
@@ -50,23 +50,30 @@ func newFlagSet(cmd, args, desc string) {
 
 func main() {
 	log.SetFlags(0)
-	flag.Parse()
 	flag.Usage = func() {
 		log.Printf("Usage: pic [opts] <subcommand> [opts] [args]\n")
 		flag.PrintDefaults()
-		log.Printf("\nSubcommands:\n")
+		log.Printf("Subcommands:\n")
 		for cmd, _ := range cmds {
-			log.Printf("    %v", cmd)
+			log.Printf("  %v", cmd)
 		}
 	}
+
+	flag.Parse()
 
 	if *lib == "" {
 		*lib = piclib.Path()
 	}
 
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		return
+	}
+
 	cmd, ok := cmds[flag.Arg(0)]
 	if !ok {
 		flag.Usage()
+		return
 	}
 	cmd(flag.Arg(0), flag.Args()[1:])
 }
@@ -74,6 +81,7 @@ func main() {
 func put(cmd string, args []string) {
 	desc := "copies given files to the library path. If no args are given, reads a list of files from stdin."
 	fs := newFlagSet("put", "[FILE...]", desc)
+	fs.Parse(args)
 	rename := fs.Bool("rename", true, "true to rename files with an exif date or sha1 hash prefix")
 
 	files := args
@@ -86,7 +94,7 @@ func put(cmd string, args []string) {
 	}
 
 	for _, path := range files {
-		p := srings.TrimSpace(path)
+		p := strings.TrimSpace(path)
 		if p == "" {
 			continue
 		}
@@ -104,7 +112,7 @@ func put(cmd string, args []string) {
 		}
 		defer src.Close()
 
-		_, err := io.Copy(dst, src)
+		_, err = io.Copy(dst, src)
 		if err != nil {
 			log.Fatal(err)
 		}
