cce217cb90340153cf9a8417a36ab840cdd1a7c8
Robert Carlsen <rwcarlsen@gmail.com>
2014-05-07 22:07:50 -0500

    added doc file to piclib

---
+++ piclib/doc.go
@@ -0,0 +1,8 @@
+package piclib
+
+/*
+All pic name arguments can be either full filepaths or just the file name.  In
+all cases, only the filename is used appended to the library directory stored
+in the Path variable.  All files should be stored in the top level of the Path
+directory - NOT nested in any subdirectories.
+*/
