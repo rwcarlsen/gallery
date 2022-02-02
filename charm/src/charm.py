f15ce2f0ac637ae54c538aaf46bdc7ad9ca8e65d
Robert Carlsen <robert.carlsen@canonical.com>
2022-02-02 14:13:28 -0700

    add ability to track/see old config states
    The user can get this info via an action, and then can use it to roll
    back the config for running units/apps.

--- charm/src/charm.py
+++ charm/src/charm.py
@@ -18,15 +18,34 @@ import logging
 import tarfile
 import time
 import subprocess
+import yaml
 
 from subprocess import CalledProcessError
 from ops.charm import CharmBase, StartEvent, ConfigChangedEvent
-from ops.framework import StoredState
+from ops.framework import StoredState, Object
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
 
+class Reverter(Object):
+    def __init__(self, charm, stored_state):
+        super().__init__(charm, 'reverter')
+        self._charm = charm
+        self._stored = stored_state
+        self._stored.set_default(config_history=[])
+
+        charm.framework.observe(charm.on.revert_action, self._revert_action)
+        charm.framework.observe(charm.on.config_changed, self._on_config_changed)
+
+    def _on_config_changed(self, event: ConfigChangedEvent) -> None:
+        cfg = self._charm.model._backend.config_get()
+        self._stored.config_history.append(cfg)
+
+    def _revert_action(self, event):
+        snapshot = event.params['snapshot'] - 1
+        cfg = self._stored.config_history[snapshot]._under
+        print(yaml.dump(cfg))
 
 class PiclibCharm(CharmBase):
 
@@ -39,10 +58,12 @@ class PiclibCharm(CharmBase):
 
         self._stored.set_default(things=[])
 
-    def _restart_piclib(self, libpath='/piclib'):
+        self._reverter = Reverter(self, self._stored)
+
+    def _restart_piclib(self, libpath):
         os.system("killall pics")
         logger.debug('killing pics server ')
-        time.sleep(1)
+        time.sleep(2)
 
         # add execute permission to pics binary
         binpath = self.model.resources.fetch("pics-binary")
@@ -71,14 +92,14 @@ class PiclibCharm(CharmBase):
             tar.extractall(path=libpath)
 
     def _on_config_changed(self, event: ConfigChangedEvent) -> None:
-        libpath = '/piclib'
-        self._install_piclib(libpath)
-        self._restart_piclib(libpath=libpath)
+        # TODO: don't restart the piclib unless it was already running.
+        libpath = self.model.config['libpath']
+        self._restart_piclib(libpath)
 
     def _on_start(self, event: StartEvent) -> None:
-        libpath = '/piclib'
+        libpath = self.model.config['libpath']
         self._install_piclib(libpath)
-        self._restart_piclib(libpath=libpath)
+        self._restart_piclib(libpath)
 
 
 if __name__ == "__main__":
